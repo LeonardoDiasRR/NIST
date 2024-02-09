a5c4eb2a74686f1248ecbe1747b5de7bb15b0b75
LeonardoDiasRR <leonardodiasrr@gmail.com>
2024-02-09 17:33:36 -0400

    primeiro commit

---
+++ testenist.py
@@ -0,0 +1,23 @@
+from NIST import NIST
+import io
+
+
+arquivo = r'download\202007231646204053190120240940.nst'
+
+with open(arquivo, 'rb') as file:
+    nist_bin = file
+
+nist = NIST(arquivo)
+
+# print(type(nist_bin))
+# if isinstance(nist_bin, io.BufferedReader):
+#     print('BufferedReader')
+# else:
+#     print('formato desconhecido')
+
+# print('FS', chr( 28 ))
+# print('GS', chr( 29 ))
+# print('RS', chr( 30 ))
+# print('US', chr( 31 ))
+# CO = ':'
+# DO = '.'
