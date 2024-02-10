99ae1c8bd4ce2469f077d914a83a2f65ed8b20c8
LeonardoDiasRR <leonardodiasrr@gmail.com>
2024-02-10 19:29:11 -0400

    24-02-10 19:29

--- testenist.py
+++ testenist.py
@@ -3,7 +3,7 @@ import io
 from pathlib import Path
 
 
-arquivo = Path(__file__) / r'/download/202007231646204053190120240940.nst'
+arquivo = Path(__file__).parent / r'/download/202007231646204053190120240940.nst'
 
 with open(arquivo, 'rb') as file:
     nist_bin = file
